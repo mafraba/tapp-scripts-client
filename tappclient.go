2b6cf4fe3ba326e94b8101d87f1d439032bb37ef
Manuel Franco <mafraba@gmail.com>
2014-07-17 23:16:37 +0200

    Main client adapted to retrieve scripts

---
+++ tappclient.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+import "net/http"
+import "io/ioutil"
+import "encoding/xml"
+
+// import "encoding/json"
+import "os"
+import "log"
+import "crypto/tls"
+import "runtime"
+
+type TappConfig struct {
+	XMLName     xml.Name `xml:"tapp"`
+	ApiEndpoint string   `xml:"server,attr"`
+	LogFile     string   `xml:"log_file,attr"`
+	LogLevel    string   `xml:"log_level,attr"`
+	Certificate Cert     `xml:"ssl"`
+}
+
+type Cert struct {
+	Cert string `xml:"cert,attr"`
+	Key  string `xml:"key,attr"`
+	Ca   string `xml:"server_ca,attr"`
+}
+
+func openTappConfiguration(fileLocation string) (config TappConfig) {
+	xmlFile, err := os.Open(fileLocation)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer xmlFile.Close()
+	b, _ := ioutil.ReadAll(xmlFile)
+	// var config TappConfig
+	xml.Unmarshal(b, &config)
+	return config
+}
+
+func createClient(config TappConfig) (client *http.Client) {
+	/**
+	 * Loads Clients Certificates and creates and 509KeyPair
+	 */
+	cert, err := tls.LoadX509KeyPair(config.Certificate.Cert, config.Certificate.Key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	/**
+	 * Creates a client with specific transport configurations
+	 */
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true},
+	}
+	client = &http.Client{Transport: transport}
+	return client
+}
+
+func main() {
+
+	const configPath = "./tapp/client.xml"
+	const endPoint = "blueprint/script_characterizations?type=boot"
+
+	config := openTappConfiguration(configPath)
+	client := createClient(config)
+
+	response, err := client.Get(config.ApiEndpoint + endPoint)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s\n\n", body)
+
+	// var policy data.Policy
+	// json.Unmarshal(body, &policy)
+
+}
